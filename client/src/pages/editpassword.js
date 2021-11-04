f6f61ae78295aa430d4a255d96660a7005ad2f15
Baranaba <baranaba@vokali.com>
2021-11-04 09:27:48 +0300

    added edit password

--- client/src/pages/editpassword.js
+++ client/src/pages/editpassword.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+
+import Navbar from "../components/Navbar";
+
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
 
 function Editpassword() {
-    return (
-        <div>Edit password</div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <Container maxWidth="md">
+        <TextField
+          id="oldpassword"
+          label="Old password"
+          sx={{ m: 2 }}
+          fullWidth
+        />
+        <TextField
+          id="newpassword"
+          label="New password"
+          sx={{ m: 2 }}
+          fullWidth
+        />
+        <Button variant="contained" sx={{ m: 2 }} fullWidth>
+          Change password
+        </Button>
+      </Container>
+    </div>
+  );
 }
 
-export default Editpassword
+export default Editpassword;
