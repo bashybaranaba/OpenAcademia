3390530cbc693f9905a3097ad8a3fec80ab76af2
bashybaranaba <33184082+bashybaranaba@users.noreply.github.com>
2022-01-31 20:48:22 +0300

    Delete keys.js

--- server/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://Baranaba:DUpE7XBLfaXw4Yh@open-academia-il.km5ae.mongodb.net/OpenAcadamia?retryWrites=true&w=majority",
-    JWT_SECRET:"openacademia2021",
-} 
